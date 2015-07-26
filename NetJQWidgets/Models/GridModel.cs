951ea2b11c939f263573900bc9fdb2e926417bf8
sunkaixuan <610262374@qq.com>
2015-07-26 16:36:57 +0800

    OK

---
+++ NetJQWidgets/Models/GridModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace NetJQWidgets.Models
+{
+    public class Data
+    {
+        public string name { get; set; }
+        public string productname { get; set; }
+        public bool available { get; set; }
+        public DateTime date { get; set; }
+        public int quantity { get; set; }
+    }
+}
